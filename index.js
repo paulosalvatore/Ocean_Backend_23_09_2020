6bc37b402d13cdece645999836da4f872a95fbd6
Paulo Salvatore <paulo@salvatore.tech>
2020-09-23 16:36:37 -0300

    MongoDB importado e conexão adicionada

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongodb = require('mongodb');
+
+// Criando um bloco assíncrono
+(async () => {
+
+// Se for uma aplicação real e um banco que você quer proteger a senha, não coloque essas informações no
+// GitHub. Procure sempre utilizar as variáveis de ambiente para isso.
+const connectionString = 'mongodb+srv://admin:Uu8ftSpPxbyJiNk@cluster0.4hryz.mongodb.net/ocean_mongodb?retryWrites=true&w=majority';
+
+// async/await
+
+console.info('Conectando ao banco de dados MongoDB!');
+
+const client = await mongodb.MongoClient.connect(connectionString, {
+    useUnifiedTopology: true
+});
+
+console.info('Banco de dados conectado com sucesso!');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -41,3 +59,6 @@ app.delete('/mensagens/:id', (req, res) => {
 app.listen(port, () => {
     console.log(`App rodando em http://localhost:${port}`);
 });
+
+// Fecho o bloco assíncrono e executo
+})();
